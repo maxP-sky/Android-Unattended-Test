30f264e1de773301522b22c7ef5ce8bb7e95ea45
Pearson <max.pearson@sky.uk>
2018-09-10 13:12:57 +0100

    init

---
+++ app/src/main/java/com/example/mpe12/loyaltyrewards/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mpe12.loyaltyrewards
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
